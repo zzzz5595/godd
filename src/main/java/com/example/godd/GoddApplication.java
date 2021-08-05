1fafd7c07692c6c504544348b353fe7b5899e709
hyn <zzzz5595@naver.com>
2021-08-05 15:56:52 +0900

    Initial commit

---
+++ src/main/java/com/example/godd/GoddApplication.java
@@ -0,0 +1,13 @@
+package com.example.godd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoddApplication.class, args);
+    }
+
+}
